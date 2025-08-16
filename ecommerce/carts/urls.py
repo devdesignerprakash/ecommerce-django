d1e07a63895309fcb8c8136fc3b0971b66c1743e
devdesignerprakash <devdesignerprakash@gmail.com>
2025-08-16 17:16:23 +0545

    @feature cart-item total vat amount grand_total items increment decrement completed

--- ecommerce/carts/urls.py
+++ ecommerce/carts/urls.py
@@ -4,5 +4,8 @@ from .views import*
 
 urlpatterns = [
     path('',cartView,name="cart"),
-    path('add_cart/<int:product_id>/', add_cart, name='add_cart')
+    path('add_cart/<int:product_id>/', add_cart, name='add_cart'),
+    path('remove_item/<int:product_id>/', remove_item, name='remove_item'),
+    path('remove_cart_item/<int:product_id>/',remove_cart_item,name='remove_cart_item')
+
 ]
--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -3,6 +3,7 @@ from store.models import Product
 from carts.models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 import uuid
+from django.shortcuts import get_object_or_404
 
 def _create_cart(request):
     cart= request.session.get('cart_id')
@@ -34,18 +35,43 @@ def add_cart(request, product_id):
     # Redirect after handling cart item
     return redirect('cart')  # make sure this URL name exists
 
+
+def remove_item(request,product_id):
+    cart=Cart.objects.get(cart_id=_create_cart(request))
+    product=get_object_or_404(Product,id=product_id)
+    cart_item=CartItem.objects.get(product=product,cart=cart)
+    if cart_item.quantity >1:
+        cart_item.quantity -=1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+def remove_cart_item(request, product_id):
+    cart=Cart.objects.get(cart_id=_create_cart(request))
+    product=get_object_or_404(Product, id=product_id)
+    cart_item=CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
+
 def cartView(request, total=0, cart_items=None):
     try:
         cart_items= CartItem.objects.filter(cart__cart_id=_create_cart(request))
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
-           
+        vat_percentage=13
+        vat_amount=(total*vat_percentage)/100
+        grand_total=total+vat_amount
+
             # quantity += cart_item.quantity
     except ObjectDoesNotExist:
         pass
     context={
         'total':total,
-        'cart_items':cart_items
+        'cart_items':cart_items,
+        'vat_amount':vat_amount,
+        'grand_total':grand_total
     }
     return render(request, 'store/cart.html',context)
 
